ca72833c313b8321b6f424abb4c0f6fc940889b5
Sriram-Kommmana <kommanamarirs@gmail.com>
2025-03-25 22:59:51 +0530

    test to see whether env is pushed or not

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -16,7 +16,7 @@ export const verifyJWT = asyncHandler(async(req, _,next)=> {
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
         if(!user){
-            //Todo: discuss about frontend
+            //Todo: discuss about frontend //idk what to do here
             throw new ApiError(401, "Invalid Access Token")
         }
         req.user = user;
